e47692c389ca8bd8a6c635c6e9209645de76cc34
Maacaa0 <102512313+Maacaa0@users.noreply.github.com>
2022-12-13 14:18:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const btn = document.querySelector(".menu_btn");
 const mobileMenu = document.querySelector(".mobile_menu");
+const scrollBtn = document.querySelector(".scroll_btn");
 
 btn.addEventListener("click", function() {
-
     mobileMenu.classList.toggle("flex");
-})
+});
 
 
-
-// I'm using "click" but it works with any event
+// handle click outside menu to close menu 
 document.addEventListener('click', event => {
   const isClickInside = mobileMenu.contains(event.target);
   const btnClick = btn.contains(event.target);
@@ -19,4 +18,24 @@ document.addEventListener('click', event => {
   } else if (!isClickInside) {
     mobileMenu.classList.remove("flex");
   }
-})
+});
+
+// scroll to display scroll to top btn
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > 900) {
+    scrollBtn.classList.remove("hidden");
+  } else {
+    scrollBtn.classList.add("hidden")
+  }
+});
+
+// take me to top btn
+scrollBtn.addEventListener("click", function() {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+});
